e654621c14fd4898a236ce5fa4fad95e72ff981d
Lobov.EM <lobov.em@gmail.com>
2023-10-24 13:48:49 +0300

    Fixed bugs

--- homeWork6/src/main.ts
+++ homeWork6/src/main.ts
@@ -2,59 +2,80 @@
 // У кожної фігури є загальнодоступні властивості - колір і назва, які не можна змінювати після створення.
 // У Square і Rectangle зі свого боку є ще додатковий метод print, який виводить рядок із формулою розрахунку площі
 interface IUtills {
-  color: string;
-  name: string;
-  calculateArea(base1: number, base2: number): number;
+  readonly color: string;
+  readonly name: string;
+  calculateArea(): number;
 }
 
 abstract class Utills implements IUtills {
-  public calculateArea(base1: number, base2: number) {
-    return base1 * base2;
-  }
+  abstract calculateArea(): number;
 
-  constructor(public readonly color: string, public readonly name: string) {}
+  constructor(public readonly color: string, public name: string) {
+    this.color = color;
+  }
 }
 
 //круг
 class Circle extends Utills {
-  override calculateArea(r: number): number {
-    return 3.14 * (r * r);
+  constructor(color: string, name: string, public radius: number) {
+    super(color, name);
+  }
+
+  calculateArea(): number {
+    return Math.PI * (this.radius * this.radius);
   }
 }
 
 //Прямоугольник
-class Rectangle extends Utills {}
+class Rectangle extends Utills {
+  constructor(color: string, name: string, public side1: number, public side2: number) {
+    super(color, name);
+  }
+
+  calculateArea(): number {
+    return this.side1 * this.side2;
+  }
+}
 
 //Квадрат
 class Square extends Utills {
-  public calculateArea(base1: number): number {
-    return base1 * base1;
+  constructor(color: string, name: string, public side1: number) {
+    super(color, name);
   }
+
+  calculateArea(): number {
+    return this.side1 * this.side1;
+  }
+
   print(): string {
-    return 'S = base*2';
+    return 'S = base^2';
   }
 }
 
 //Треугольник
 class Triangle extends Utills {
-  public calculateArea(base: number, hight: number): number {
-    return (base * hight) / 2;
+  constructor(color: string, name: string, public side1: number, public hight: number) {
+    super(color, name);
+  }
+
+  calculateArea(): number {
+    return (this.side1 * this.hight) / 2;
   }
 
   print(): string {
-    return 'S = (base * hight) / 2';
+    return 'S = (side1 * hight) / 2';
   }
 }
 
-const circle = new Circle('white', 'circle');
-const rectangle = new Rectangle('white', 'rectangle');
-const square = new Square('Green', 'Square');
-const triangle = new Triangle('black', 'Triangle');
+const circle = new Circle('white', 'circle', 10);
+const rectangle = new Rectangle('white', 'rectangle', 10, 20);
+const square = new Square('Green', 'Square', 10);
+const triangle = new Triangle('black', 'Triangle', 10, 20);
 
-console.log(circle.calculateArea(10));
-console.log(rectangle.calculateArea(20, 20));
-console.log(square.calculateArea(10));
-console.log(triangle.calculateArea(10, 20));
+console.log(circle.calculateArea());
+console.log(rectangle.calculateArea());
+console.log(square.calculateArea());
+console.log(triangle.calculateArea());
 
 console.log(square.print());
 console.log(triangle.print());
