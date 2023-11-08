e6a831fee18303e29cff14f53514927f646332b3
Lobov.EM <lobov.em@gmail.com>
2023-11-08 16:02:29 +0200

    Fixed bug

---
+++ homeWork9(mappedType)/src/main.ts
@@ -0,0 +1,146 @@
+// MAPPED  types
+// // Вам потрібно створити тип DeepReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів.
+interface IUser {
+  name: string;
+  user1: {
+    name: string;
+    surname: string;
+  };
+  user2: {
+    name: string;
+    surname: string;
+  };
+}
+
+//рекурсия для вложенных объектов
+type DeepReadonly<T> = {
+  //below examples
+  // readonly [name in keyof IUser]?: <T[K] = string>;
+  // readonly [user1 in keyof IUser]?: DeepReadonly<T[K] = user1>;
+  // readonly [name in keyof user1]?: DeepReadonly<T[K] = string>;
+
+  // readonly [K in keyof T]?: DeepReadonly<T[K]>;
+  readonly [K in keyof T]?: T[K] extends object ? DeepReadonly<T[K]> : T[K];
+};
+
+// const user: Readonly<IUser> = {
+//   name: 'Jon',
+//   surname: 'Rod',
+// };
+
+const userFirst: DeepReadonly<IUser> = {
+  name: 'Test',
+  user1: { name: 'Jon', surname: 'Rod' },
+};
+
+if (userFirst.user1) {
+  // userFirst.user1.name = '123';
+}
+
+// userFirst.name = '222';
+console.log(userFirst);
+
+// Вам потрібно створити тип DeepRequireReadonly який буде робити доступними тільки для читання навіть властивості вкладених обʼєктів
+// та ще й робити їх обовʼязковими.
+type DeepRequireReadonly<T> = {
+  readonly [K in keyof T]-?: T[K] extends object ? DeepRequireReadonly<T[K]> : T[K];
+};
+
+const userSecond: DeepRequireReadonly<IUser> = {
+  name: 'Test',
+  user1: { name: 'Jon', surname: 'Rod' },
+  user2: { name: 'Mark', surname: 'Kuk' },
+};
+
+// userSecond.name = '000';
+// userSecond.user1.name = '9999';
+console.log(userSecond);
+
+// Вам потрібно сворити тип UpperCaseKeys, який буде приводити всі ключи до верхнього регістру.
+interface IUserSec {
+  name: string;
+  surname: string;
+}
+
+type UpperCaseKeys<T> = {
+  [K in keyof T as Uppercase<string & K>]: T[K];
+};
+
+const user: UpperCaseKeys<IUserSec> = {
+  NAME: 'Bob',
+  SURNAME: 'Villi',
+};
+// const user = {
+//   name: 'Bob',
+//   surname: 'Villi',
+// };
+
+// type UpperCaseObj = UpperCaseKeys<typeof user>;
+
+console.log(user);
+
+// І саме цікаве. Створіть тип ObjectToPropertyDescriptor, який перетворює звичайний обʼєкт на обʼєкт де кожне value є дескриптором.
+interface IUserThird {
+  name: string;
+  surname: string;
+}
+
+type ObjectToPropertyDescriptor<T> = {
+  [K in keyof T]: PropertyDescriptor;
+};
+
+// const userThird: ObjectToPropertyDescriptor<IUserThird> = {
+//   name: {
+//     value: 'Vil',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true,
+//   },
+//   surname: {
+//     value: 'Villy',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true,
+//   },
+// };
+
+const obj: IUserThird = {
+  name: 'Bob',
+  surname: 'AsdBob',
+};
+
+const userThird: ObjectToPropertyDescriptor<typeof obj> = {
+  name: Object.getOwnPropertyDescriptor(obj, 'name')!,
+  surname: Object.getOwnPropertyDescriptor(obj, 'surname')!,
+};
+
+console.log(userThird);
+
+// const userThird: ObjectToPropertyDescriptor<IUserThird> = {
+//   name: { value: '212', writable: false },
+// };
+
+// Object.defineProperty(userThird, 'name', {
+//   value: 'Bob',
+//   writable: faslse,
+// });
+
+// userThird.name = '123';
+
+// Object.defineProperty(userThird, 'name', {
+//   value: 'Bob',
+//   writable: true,
+//   configurable: true,
+// });
+
+// userThird.name.value = '123';
+
+// const obj = {};
+// Object.defineProperty(obj, 'name', {
+//   value: 'John',
+//   writable: false, // Заборонити зміну значення
+//   enumerable: true, // Дозволити перебір
+//   configurable: false, // Заборонити зміну конфігурації
+// });
+
+// console.log(obj);
