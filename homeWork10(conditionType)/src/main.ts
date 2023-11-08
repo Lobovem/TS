9cf48c7141283e205bc403e4c7df880fd10b4394
Lobov.EM <lobov.em@gmail.com>
2023-11-08 18:38:31 +0200

    I done first task that is GetReturnType

--- homeWork10(conditionType)/src/main.ts
+++ homeWork10(conditionType)/src/main.ts
@@ -1,10 +1,23 @@
 // Вам потрібно створити умовний тип, що служить для встановлення типу, що повертається з функції.
 // Як параметр типу повинен обов'язково виступати функціональний тип.
-type TypeA<T> = T extends (param: infer U) => void ? U : undefined;
+type GetReturnType<T> = T extends (...ars: never[]) => infer Return ? Return : never;
 
-function funcA(param: number): void {}
+//Parameters of type  is function
+type TypeNum = GetReturnType<() => number>;
+type TypeStr = GetReturnType<() => string>;
 
-let A: TypeA<typeof funcA>;
+//example with some function
+function getSum(a: number, b: number): number {
+  return a + b;
+}
+
+//
+type GetNumType = GetReturnType<typeof getSum>;
+let numValue: GetNumType = getSum(10, 2);
+let numValue2: GetNumType = getSum(2, 2);
+console.log(numValue);
 
 // Вам потрібно створити умовний тип, який приймає функціональний тип з одним параметром (або задовільним)
 // та повертає кортеж, де перше значення - це тип, що функція повертає, а другий - тип її параметру
+
+type GetType<T> = T extends (param: infer U) => infer Return ? Return : never ? U :undefined;
